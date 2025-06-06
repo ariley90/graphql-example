d4336830b9e99925f49d29dd64c07bb76ecdae88
Adam Riley <ariley1990@gmail.com>
2025-06-06 19:00:42 +0100

    add next bundle analysis

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+import  withBundleAnalyzer from '@next/bundle-analyzer';
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -7,4 +9,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig);
